4d3a189c0d47f6439cc80ddc4c9518ee2a9412d2
Ansiya-tk <ansiyatk@gmail.com>
2019-12-14 15:43:35 +0530

    project is pushed

---
+++ src/AxisBank.java
@@ -0,0 +1,21 @@
+
+public class AxisBank extends BankInfo {
+//	public void deposit(int b)
+//	{
+//		System.out.println("This is child class");
+//	}
+   
+	public static void main(String[] args) {
+		AxisBank ab=new AxisBank();
+		ab.deposit(3);
+		ab.savings();
+		ab.fixed();
+	}
+
+	@Override
+	public void deposit(int g) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("From child class");
+	}
+}
---
+++ src/BankInfo.java
@@ -0,0 +1,17 @@
+
+public abstract class BankInfo {
+//               public void deposit(int a)
+//               {
+//            	   System.out.println("From base class");
+//               }
+	   public abstract void deposit(int g);
+	   
+               public void savings()
+               {
+            	   System.out.println("Savings accout");
+               }
+               public void fixed()
+               {
+            	   System.out.println("Fixed deposit");
+               }
+}
